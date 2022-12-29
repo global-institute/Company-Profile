a71f60c0449197541cd6fa2be7be0f6207bca329
dhewdkmlh <118949656+dhewdkmlh@users.noreply.github.com>
2022-12-29 17:37:13 +0700

    Delete dhea.js

--- html/team/json/dhea.js
+++
@@ -1,8 +0,0 @@
-{
-    "nama": "Dhea Wida Kamilah",
-    "quote": "Haloo guyssss",
-    "image": "https://magdalene.co/storage/media/Doraemon3%20copy.jpg",
-    "whatsapp": "#",
-    "facebook": "#",
-    "twitter": "#"
-}
